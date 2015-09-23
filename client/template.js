949cb46c336e42d33f1d2bd06ebfb8c3379b592f
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-09-23 01:36:47 -0700

    compiling main client js file

--- client/template.js
+++ client/template.js
@@ -4,9 +4,7 @@ var riot = require('riot'),
     appView = require('./app.tag'),
     bulk = require('bulkify');
 
-require('../static/wireframe-ui/wireframe-ui-header/template.tag');
-require('../static/wireframe-ui/wireframe-ui-list/template.tag');
-require('../static/wireframe-ui/wireframe-ui-navigation/template.tag');
+// ---- //
 
 module.exports = {
     start : start
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var express = require('express'),
     path = require('path'),
     fs = require('fs'),
     _ = require('lodash'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    execSync = require('child_process').execSync;
 
 module.exports = {
     start: start
@@ -18,13 +19,27 @@ function start(options) {
         port = options.port || 3000,
         child,
         wireFrameUiJSON = require(path.join(baseDirectory, '../wireframe-ui.json')),
-        sassFile = '@import "main";\n';
+        jsTemplate = '' + fs.readFileSync(path.join(__dirname, 'client', 'template.js')),
+        sassFile = '@import "main";\n',
+        jsIncludes = '';
 
     _.forEach(wireFrameUiJSON.views, function(view) {
         sassFile    += '@import "' + path.join(__dirname,  'static', 'wireframe-ui', view, 'style.scss') + '";\n';
+        jsIncludes  += "require('../static/wireframe-ui/" + view + "/template.tag');\n";
     });
 
     fs.writeFileSync(path.join(__dirname, 'sass', 'compiled.scss'), sassFile);
+    fs.writeFileSync(path.join(__dirname, 'client', 'index.js'), jsTemplate.replace('// ---- //', jsIncludes))
+
+    execSync('bower install', {
+        cwd : __dirname
+    });
+
+    _.forEach(wireFrameUiJSON.views, function(view) {
+        execSync('bower install ' + view, {
+            cwd : __dirname
+        });
+    });
 
     child = exec(__dirname + '/bin/start ' + baseDirectory + ' ' + __dirname);
 
